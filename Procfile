4864ba92e3249eb4ee8672080edae8717b4479c3
devrasmeriya <67556120+devrasmeriya@users.noreply.github.com>
2020-07-26 14:07:34 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn new_app:app
---
+++ new_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+# from new_app.models import info,Document,student
+
+# Register your models here.
+# admin.site.register(info)
+# admin.site.register(Document)
+# admin.site.register(student)
